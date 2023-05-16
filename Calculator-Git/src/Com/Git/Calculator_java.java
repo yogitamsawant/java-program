bb953113c4b0d4fa6b44a633bffb12f8e30a1b59
yogitamsawant <133479967+yogitamsawant@users.noreply.github.com>
2023-05-15 22:37:05 -0700

    Update Calculator_java.java
    Add new Subtraction method

--- Calculator-Git/src/Com/Git/Calculator_java.java
+++ Calculator-Git/src/Com/Git/Calculator_java.java
@@ -2,8 +2,6 @@ package Com.Git;
 
 public class Calculator_java 
 {
-	
-	
 	public void add()
 	{
 		System.out.println("Addition");
@@ -12,12 +10,21 @@ public class Calculator_java
 		int c= a+b;
 		System.out.println("Addition is :" +c);
 	}
+	public void sub()
+	{
+		System.out.println("Subtraction");
+		int x=20;
+		int y=10;
+		int z= x-y;
+		System.out.println("Subtraction is :" +z);
+		
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Main Process");
 		 Calculator_java cj= new Calculator_java();
 		 cj.add();
-		
+		 cj.sub();
 	}
 
 }
