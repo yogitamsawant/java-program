85d1be6594467e3cd846f966fef658fe2e1c67f9
admin <admin@DESKTOP-UVM6947>
2023-05-16 10:53:56 -0700

    By Yogita

---
+++ Calculator-Git/src/Com/Git/Calculator_java.java
@@ -0,0 +1,22 @@
+package Com.Git;
+
+public class Calculator_java 
+{
+	
+	
+	public void add()
+	{
+		System.out.println("Addition");
+		int a=10;
+		int b=20;
+		int c= a+b;
+		System.out.println("Addition is :" +c);
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Main Process");
+	}
+
+}
